27ad5fa893f3d7a0b7e08866bc5769357ec61a12
Nanna Ketola <nannailona@gmail.com>
2025-06-10 16:43:48 +0300

    Exercise 1.7

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -8,30 +8,58 @@ const Button = ({ onClick, text }) => {
   )
 }
 
-const Display = ({ text, value }) => {
-  return (
-    <p>
-      {text} {value}
-    </p>
-  )
+const Average = ({ total, all }) => {
+  if (all === 0) {
+    return <p>average 0</p>
+  }
+  return <p>average {total / all}</p>
+}
+
+const Positive = ({ good, all }) => {
+  if (all === 0) {
+    return <p>positive 0 %</p>
+  }
+  return <p>positive {good / all * 100} %</p>
 }
 
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [total, setTotal] = useState(0)
+
+  const handleGoodClick = () => {
+    setGood(good + 1)
+    setAll(all + 1)
+    setTotal(total + 1)
+  }
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+    setAll(all + 1)
+  }
+
+  const handleBadClick = () => {
+    setBad(bad + 1)
+    setAll(all + 1)
+    setTotal(total - 1)
+  }
 
   return (
     <div>
       <h1>Give Feedback</h1>
-      <Button onClick={() => setGood(good + 1)} text='good' />
-      <Button onClick={() => setNeutral(neutral + 1)} text='neutral' />
-      <Button onClick={() => setBad(bad + 1)} text='bad' />
+      <Button onClick={handleGoodClick} text='good' />
+      <Button onClick={handleNeutralClick} text='neutral' />
+      <Button onClick={handleBadClick} text='bad' />
 
       <h1>Statistics</h1>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <Average total={total} all={all}/>
+      <Positive good={good} all={all} />
     </div>
   )
 }
