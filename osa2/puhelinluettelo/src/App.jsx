a258031e54e22d5e6e3262854525cd7aa4cbcd5d
Nanna Ketola <nannailona@gmail.com>
2025-06-26 14:54:39 +0300

    Exercise 2.7

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -15,12 +15,15 @@ const App = () => {
   const addName = (event) => {
     event.preventDefault()
 
-    const nameObject = {
-      name: newName
+    if (persons.some((person) => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`)
+    } else {
+      const nameObject = {
+        name: newName
+      }
+      setPersons(persons.concat(nameObject))
+      setNewName('')
     }
-
-    setPersons(persons.concat(nameObject))
-    setNewName('')
   }
 
   const handleNameChange = (event) => {
