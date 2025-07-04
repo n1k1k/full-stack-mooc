4f1116fcc6cbd27600e6c28adb0f7f6684d679f6
Nanna Ketola <nannailona@gmail.com>
2025-07-04 09:42:59 +0300

    Exercise 2.16

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -1,4 +1,6 @@
 import { useState, useEffect } from 'react'
+import './index.css'
+import Notification from './components/Notification'
 import Person from './components/Person'
 import PersonForm from './components/PersonFrom'
 import personService from './services/persons'
@@ -10,6 +12,7 @@ const App = () => {
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter ] = useState('')
+  const [message, setMessage] = useState(null)
   const personsToShow = persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
 
   useEffect(() => {
@@ -32,6 +35,12 @@ const App = () => {
           .update(person.id, changedPerson)
           .then(returnedPerson => {
             setPersons(persons.map(p => p.id !== person.id ? p : returnedPerson))
+            setNewName('')
+            setNewNumber('')
+            setMessage(`Updated number for ${newName}`)
+            setTimeout(() => {
+              setMessage(null)
+            }, 5000)
           })
 
       }
@@ -47,6 +56,10 @@ const App = () => {
           setPersons(persons.concat(returnedPerson))
           setNewName('')
           setNewNumber('')
+          setMessage(`Added ${newName}`)
+          setTimeout(() => {
+            setMessage(null)
+          }, 5000)
         })
     }
   }
@@ -60,6 +73,10 @@ const App = () => {
       .deletePerson(id)
       .then(returnedPerson => {
         setPersons(persons.filter(p => p.id !== returnedPerson.id))
+        setMessage(`Deleted ${returnedPerson.name}`)
+            setTimeout(() => {
+              setMessage(null)
+            }, 5000)
       })
     }
   }
@@ -79,6 +96,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={message} />
       <Filter filter={filter} handleFilterChange={handleFilterChange} />
       <h3>Add new</h3>
       <PersonForm addName={addName} newName={newName} newNumber={newNumber} handleNameChange={handleNameChange} handleNumberChange={handleNumberChange}/>
