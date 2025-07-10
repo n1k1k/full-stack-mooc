d50acee6b4171f3ff07425f6180054b23782a197
Nanna Ketola <nannailona@gmail.com>
2025-07-10 14:27:11 +0300

    Exercise 3.3

--- osa3/puhelinluettelo_backend/index.js
+++ osa3/puhelinluettelo_backend/index.js
@@ -29,7 +29,6 @@ app.get('/', (request, response) => {
     response.send('<h1>Puhelinluettelo</h1>')
 })
 
-
 app.get('/info', (request, response) => {
     const date = new Date().toString()
 
@@ -44,6 +43,17 @@ app.get('/info', (request, response) => {
 
 app.get('/api/persons', (request, response) => {
     response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    const person = persons.find(person => person.id === id)
+
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
 })
 
 const PORT = 3001
