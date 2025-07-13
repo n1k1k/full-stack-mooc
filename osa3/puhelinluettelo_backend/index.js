d69c805cce094a65203e03f4f021837ac3813b2f
Nanna Ketola <nannailona@gmail.com>
2025-07-13 19:18:07 +0300

    uibuild

--- osa3/puhelinluettelo_backend/index.js
+++ osa3/puhelinluettelo_backend/index.js
@@ -1,13 +1,11 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
-const cors = require('cors')
 
 morgan.token('body', (req) => {
   return req.method === 'POST' ? JSON.stringify(req.body) : ''
 })
 
-app.use(cors())
 app.use(express.json())
 app.use(express.static('dist'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
