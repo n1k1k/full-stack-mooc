31054e25f1d0a19d8265e0aa6426b61784e7fad2
Nanna Ketola <nannailona@gmail.com>
2025-07-17 10:23:01 +0300

    Exercises 3.15 and 3.16

--- osa3/puhelinluettelo_backend/index.js
+++ osa3/puhelinluettelo_backend/index.js
@@ -9,9 +9,25 @@ morgan.token('body', (req) => {
   return req.method === 'POST' ? JSON.stringify(req.body) : ''
 })
 
-app.use(express.json())
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformed id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).send({ error: error.message })
+    } else if (error.name === 'BadRequest') {
+        return response.status(400).send({ error: error.message })
+    }
+
+    next(error)
+}
+
 app.use(express.static('dist'))
+app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+let persons = []
 
 const getRandomId = (max) => {
     return String(Math.floor(Math.random()* max))
@@ -40,33 +56,40 @@ app.get('/api/persons', (request, response) => {
 })
 
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
     const id = request.params.id
     Person.findById(id).then((person) => {
-        response.json(person)
+        if (person) {
+            response.json(person)
+        } else {
+            console.log()
+            response.status(404).end()
+        }
     })
+    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    persons = persons.filter(person => person.id !== id)
-
-    response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
 
     if (!body.name) {
-        return response.status(400).json({
-            error: 'name is missing'
-        })
+        const error = new Error('name is missing')
+        error.name = 'BadRequest'
+        throw error
     }
     
     if (!body.number) {
-        return response.status(400).json({
-            error: 'number is missing'
-        })
+        const error = new Error('number is missing')
+        error.name = 'BadRequest'
+        throw error
     }
     
     
@@ -84,8 +107,11 @@ app.post('/api/persons', (request, response) => {
 
     person.save().then(savedPerson => {
         response.json(savedPerson)
-    })
+        })
+        .catch(error => next(error))
 })
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
